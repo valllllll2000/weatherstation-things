dd2394a2d2ecd7e1407002750c2fc80701ccfa98
valeria <valerie.cardoso@gmail.com>
2018-06-09 18:30:05 +0200

    Adjusting temperature by 0.6 to take into account cpu temp.

--- app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.kt
+++ app/src/main/java/com/example/androidthings/weatherstation/WeatherStationActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.androidthings.weatherstation
 
+import android.Manifest
 import android.app.Activity
 import android.graphics.Color
 import android.hardware.Sensor
@@ -23,13 +24,15 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.HardwarePropertiesManager.*
 import android.util.Log
 import com.google.android.things.contrib.driver.apa102.Apa102
 import com.google.android.things.contrib.driver.bmx280.Bmx280SensorDriver
 import com.google.android.things.contrib.driver.ht16k33.AlphanumericDisplay
 import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
 import java.io.IOException
-
+import java.io.RandomAccessFile
+import android.content.pm.PackageManager
 
 class WeatherStationActivity : Activity() {
 
@@ -37,8 +40,9 @@ class WeatherStationActivity : Activity() {
     private var ledsStrip: Apa102? = null
     private var sensorDriver: Bmx280SensorDriver? = null
     private var sensorManager: SensorManager? = null
+    private var cpuTemperature: Float = -1f
 
-    private val mSensorEventListener = object : SensorEventListener {
+    private val sensorEventListener = object : SensorEventListener {
 
         override fun onSensorChanged(event: SensorEvent) {
             val value = event.values[0]
@@ -49,6 +53,10 @@ class WeatherStationActivity : Activity() {
             if (event.sensor.type == Sensor.TYPE_PRESSURE) {
                 updateBarometerDisplay(value)
             }
+            if (event.sensor.type == Sensor.TYPE_TEMPERATURE) {
+                Log.d(TAG, "temperature of cpu is: $value")
+                cpuTemperature = value
+            }
         }
 
         override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -56,10 +64,11 @@ class WeatherStationActivity : Activity() {
         }
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Weather Station Started")
+
+        //checkCpuTemperature()
 
         try {
             sensorDriver = RainbowHat.createSensorDriver()
@@ -79,9 +88,8 @@ class WeatherStationActivity : Activity() {
 
         try {
             ledsStrip = RainbowHat.openLedStrip()
-            ledsStrip?.brightness = LEDSTRIP_BRIGHTNESS
+            ledsStrip?.brightness = DEFAULT_LED_STRIP_BRIGHTNESS
             val colors = intArrayOf(Color.RED, Color.GREEN, Color.RED, Color.BLUE, Color.RED, Color.DKGRAY, Color.YELLOW)
-            //            Arrays.fill(colors, Color.RED);
             ledsStrip?.write(colors)
             ledsStrip?.write(colors) //write twice due to some issue
         } catch (e: IOException) {
@@ -89,26 +97,57 @@ class WeatherStationActivity : Activity() {
         }
 
         sensorManager = getSystemService(SensorManager::class.java)
+    }
 
+    private fun checkCpuTemperature() {
+        val permission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            requestPermissions(
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            )
+        }
+        var reader: RandomAccessFile? = null
+        try {
+            reader = RandomAccessFile(CPU_FILE_PATH, "r")
+            val rawTemperature = reader.readLine()
+            cpuTemperature = java.lang.Float.parseFloat(rawTemperature) / 1000f
+            Log.i(TAG, "Parsed temp: $cpuTemperature")
+        } catch (e: IOException) {
+            Log.e(TAG, "Error reading cpu temp", e)
+        } finally {
+            try {
+                reader?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Error closing reader", e)
+            }
+
+        }
+        //val manager = getSystemService(Context.HARDWARE_PROPERTIES_SERVICE) as HardwarePropertiesManager
+        //val temperature = manager.getDeviceTemperatures(TEMPERATURE_CURRENT, DEVICE_TEMPERATURE_CPU)
+
+        //cpuTemperature = temperature[0]
+
+        Log.i(TAG, "Parsed temp: $cpuTemperature")
     }
 
     override fun onStart() {
         super.onStart()
 
-        // Register the BMP280 temperature sensor
-        val temperature = sensorManager?.getDynamicSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE)?.get(0)
-        sensorManager?.registerListener(mSensorEventListener, temperature,
+        val temperatureSensor = sensorManager?.getDynamicSensorList(Sensor.TYPE_AMBIENT_TEMPERATURE)?.get(0)
+        sensorManager?.registerListener(sensorEventListener, temperatureSensor,
                 SensorManager.SENSOR_DELAY_NORMAL)
 
-        // Register the BMP280 pressure sensor
-        val pressure = sensorManager?.getDynamicSensorList(Sensor.TYPE_PRESSURE)?.get(0)
-        sensorManager?.registerListener(mSensorEventListener, pressure,
+        val pressureSensor = sensorManager?.getDynamicSensorList(Sensor.TYPE_PRESSURE)?.get(0)
+        sensorManager?.registerListener(sensorEventListener, pressureSensor,
                 SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onStop() {
         super.onStop()
-        sensorManager?.unregisterListener(mSensorEventListener)
+        sensorManager?.unregisterListener(sensorEventListener)
     }
 
     override fun onDestroy() {
@@ -122,6 +161,7 @@ class WeatherStationActivity : Activity() {
                 Log.e(TAG, "Error closing display", e)
             } finally {
                 display = null
+
             }
         }
 
@@ -154,7 +194,8 @@ class WeatherStationActivity : Activity() {
     private fun updateTemperatureDisplay(temperature: Float) {
         try {
             Log.d(TAG, "temperature: $temperature")
-            display!!.display(temperature.toString())
+            val temp = temperature * 0.6
+            display!!.display(temp.toString())
         } catch (e: IOException) {
             Log.e(TAG, "Error updating display", e)
         }
@@ -175,9 +216,12 @@ class WeatherStationActivity : Activity() {
     }
 
     companion object {
-        private val TAG = "WeatherStationActivity";
-
-        // Default LED brightness
-        private val LEDSTRIP_BRIGHTNESS = 1
+        private const val TAG = "WeatherStationActivity"
+        private const val DEFAULT_LED_STRIP_BRIGHTNESS = 1
+        private const val CPU_FILE_PATH = "/sys/class/thermal/thermal_zone0/temp"
+        private const val HEATING_COEFFICIENT = 0.55f
+        private const val UPDATE_CPU_DELAY: Long = 50
+        private const val REQUEST_EXTERNAL_STORAGE = 1
+        private val PERMISSIONS_STORAGE = arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE)
     }
 }
